edb5bbf51fb4f150b33bc6363b726a0858ca62ec
Mali Scheuer <youremail@gmx.net>
2021-02-03 19:03:53 +0100

    added some js

--- src/index.js
+++ src/index.js
@@ -1,39 +1,28 @@
-//const button1 = document.querySelector('#button1')
-//const answer1 = document.querySelector('#answer1')
+/*
 
-//const button2 = document.querySelector('#button2')
-//const answer2 = document.querySelector('#answer2')
-
-//const button3 = document.querySelector('#button3')
-//const answer3 = document.querySelector('#answer3')
-
-//const button4 = document.querySelector('#button4')
-//const answer4 = document.querySelector('#answer4')
+const button1 = document.querySelector('#button1')
+const answer1 = document.querySelector('#answer1')
 
 
-
-/*button1.addEventListener('click', () => {
+button1.addEventListener('click', () => {
     answer1.classList.toggle('show');
-}
-)
+})
 
-button2.addEventListener('click', () => {
-    answer2.classList.toggle('show');
-}
-)
-
-button3.addEventListener('click', () => {
-    answer3.classList.toggle('show');
-}
-)
-
-button4.addEventListener('click', () => {
-    answer4.classList.toggle('show');
-}
-)
 */
 
 
+
+/* Highlight Home Icon*/
+const homeIcon = document.querySelector('.nav__home__icon')
+
+homeIcon.addEventListener('click', () => {
+    homeIcon.classList.add('filled_home_icon');
+})
+/* Highlight Home Icon*/
+
+
+
+/* Show answers */
 const answerButtons = document.querySelectorAll('.questionbox__button')
 
 answerButtons.forEach((buttons) => {
@@ -45,8 +34,21 @@ answerButtons.forEach((buttons) => {
     })
 
 })
+/*Show answers */
 
 
+/* Highlight answer */
+const button1 = document.querySelector('#button1')
+const answerTag1 = document.querySelector('.questionbox__tags__1')
+
+button1.addEventListener('click', () => {
+    answerTag1.classList.toggle('show_tag');
+})
+/* Highlight answer */
+
+
+
+/* Highlight Bookmarks */
 const iconBookmark = document.querySelectorAll('.questionbox__bookmarkicon')
 
 iconBookmark.forEach((bookmarks) => {
@@ -58,15 +60,19 @@ iconBookmark.forEach((bookmarks) => {
     })
 
 })
+/* Highlight Bookmarks */
 
 
 
+/* Highlight first Bookmark
 
-/*
 const iconBookmark = document.querySelector('.questionbox__bookmarkicon')
 
 iconBookmark.addEventListener('click', () => {
     iconBookmark.classList.toggle('filled_icon');
 }
 )
+
 */
+
+
